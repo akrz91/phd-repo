16b12f2d0a466b152d4c3d4e23fb4f1d4736c219
Adam <a.krzywaniak@gmail.com>
2018-05-02 15:05:25 +0200

    updated night_run script

--- night_run.sh
+++ night_run.sh
@@ -1,3 +1,3 @@
-./RunExperiments ../../Downloads/bench-openmp/integrate  1.0 2.0 10000000000
-./RunExperiments ../../Downloads/bench-openmp/heat 1000 0.2
-./RunExperiments ../../Downloads/bench-openmp/fft 4096
+./RunExperiments ./bench-openmp/integrate  1.0 2.0 100000000000
+./RunExperiments ./bench-openmp/heat 40000 0.2
+./RunExperiments ./bench-openmp/fft 32768
